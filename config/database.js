33b6737bf0a93c38776fc8cd5f2acad6be1b185c
Bernard Gresola <68579975+bernard-uerm@users.noreply.github.com>
2021-07-21 11:05:19 +0800

    initial commit

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ require('dotenv/config')
 
 const { Client } = require('pg');
 const pgConfig = new Client({
-	connectionString: process.env.DB_CONNECTION_STRING,
+	connectionString: 'postgres://uhuxvcowhsuiyb:cb33d48d232eca3acaf98c60137b38a9f153e4524630547e671b216f9baece8f@ec2-54-236-137-173.compute-1.amazonaws.com:5432/d5mue47ps55ckg',
 	ssl: {
 		rejectUnauthorized: false
 	}
